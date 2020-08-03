23b4df693fe663a040c5952afe389361211329d8
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-08-03 19:08:53 -0300

    test 1

---
+++ src/main/java/com/apispring/apispring/routes/UserControllerClass.java
@@ -0,0 +1,22 @@
+package com.apispring.apispring.routes;
+
+import com.apispring.apispring.models.User;
+import com.apispring.apispring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/api")
+public class UserControllerClass {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+}
