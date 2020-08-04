535785470cab9e2c9643dbc8eff72387f4ff63ac
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-08-03 21:24:41 -0300

    ts

--- src/main/java/com/apispring/apispring/routes/UserControllerClass.java
+++ src/main/java/com/apispring/apispring/routes/UserControllerClass.java
@@ -19,12 +19,12 @@ public class UserControllerClass {
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public List<User> getAllUsers() {
-        System.out.println("dasdsa");
         return userService.findAll();
     }
 
     @RequestMapping(value = "/users/add", method = RequestMethod.POST)
     public User saveUser(User user) {
+        System.out.println("dasdsa" + user);
         return userService.save(user);
     }
 
